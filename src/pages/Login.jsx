1d832e6d7ed98975ab05d096f4df09ea26c775e2
persiopeo <persiopeo@hotmail.com>
2022-04-14 16:17:14 -0300

    Validando os Input do Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      senha: '',
+
+    };
+  }
+
   render() {
+    const { email, senha } = this.state;
+    const SEIS = 6;
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
     return (
       <div>
         <input
           data-testid="email-input"
           placeholder="Email"
           type="text"
+          onChange={ ({ target: { value } }) => this.setState({ email: value }) }
         />
         <input
           data-testid="password-input"
           placeholder="Senha"
           type="password"
+          onChange={ ({ target: { value } }) => this.setState({ senha: value }) }
         />
+        <button
+          type="button"
+          disabled={ !(regex.test(email) && senha.length >= SEIS) }
+
+        >
+          Entrar
+        </button>
       </div>
 
     );
